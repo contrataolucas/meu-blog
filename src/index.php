5265911fa5714197523dbfbb99e3834dfbacc003
Lucas <contrataolucas@gmail.com>
2017-04-11 23:35:15 -0300

    index
    index

--- src/index.php
+++ src/index.php
@@ -1,12 +1,112 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title></title>
+        <title>Lucas Oliveira | Dev Front-end </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="assets/css/style.css" rel="stylesheet">
     </head>
     <body>
+        <header class="header">
+            <div class="container">
+                <a href="index.php"><h1>Lucas Oliveira <span>Desenvolvedor Web</span></h1></a>
+                <div>
+                    <nav class="menu">
+                        <ul>
+                            <li><a href="trabalhos.php">trabalhos</a></li>
+                            <li><a href="projetos.php">projetos</a></li>
+                            <li><a href="blog.php">blog</a></li>
+                        </ul>
+                    </nav>
+                    <ul>
+                        <li><a class="icon-facebook" href="#"></a></li>
+                        <li><a class="icon-linked" href="#"></a></li>
+                        <li><a class="icon-github" href="#"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <main class="conteudo">
+            <section class="sobre-min">
+                <div>
+                    <h2>Sobre min</h2>
+                    <div>
+                        <div>
+                            <div>
+                                <p>Meu nome é Lucas de Oliveira , tenho 27 anos, moro em Paulínia, São Paulo e trabalho como Desenvolvedor Front-end desde 2016. </p>
+                                <p>Atualmente trabalho como desenvolvedor front-end. Já trabalhei em empresas como Lema Agência Digital, Agência Passo , Startup Futurando, Grupo Baobás . Desenvolvi e participei de vários projetos legais, alguns deles estão aqui no meu portfolio de desenvolvedor front-end. Seja Bem vindo!</p>
+                                <p>Sempre procuro aprender algo novo e entender como as coisas funcionam, desde de uma feature até uma biblioteca ou framework. Gosto quando me deparo com um desafio enquanto desenvolvo algo, pois foi dessa forma que aprendi muito do que sei hoje.</p>
+                            </div>
+                        </div>
+                        <figure>
+                            <img src="assets/img/lucas-oliveira.jpg" alt="Lucas Oliveira">
+                        </figure>
+                    </div>
+                </div>
+            </section>
+            <section class="minhas-habilidades">
+                <h2>Minhas Habilidades:</h2>
+                <ul>
+                    <li>html5</li>
+                    <li>css3</li>
+                    <li>javascript</li>
+                    <li>react</li>
+                    <li>gulp</li>
+                    <li>sass</li>
+                    <li>php</li>
+                </ul>
+            </section>
+            <section class="ultimos-trabalhos">
+                <div>
+                    <h2>Ultimos trabalhos</h2>
+                    <div>
+                        <figure>
+                            <img src="assets/img/cliente2.jpg" alt="nome do cliente">
+                        </figure>
+                        <figure>
+                            <img src="assets/img/cliente1.jpg" alt="nome do cliente">
+                        </figure>
+                    </div>
+                </div>
+            </section>
+            <section class="ultimos-projetos">
+                <div>
+                    <h2>Ultimos projetos</h2>
 
+                </div>
+            </section>
+            <section class="postagens-recentes">
+                <div>
+                    <h2>Postagens recentes</h2>
+                    <ul>
+                        <li>
+                            <figure>
+                                <img src="assets/img/img-post.jpg" alt="imagem do blog">
+                            </figure>
+                            <div>
+                                <h3>Guia html5 para iniciantes</h3>
+                                <time datetime="2017-04-29T19:00">20 JUN 2016</time>
+                                <p>O HTML é uma linguagem de códigos usada para marcação e estruturação de conteúdo. Neste tutorial, irei falar um pouco sobre a estruturação básica das páginas web utilizando HTML.</p>
+                                <a href="#">Post completo</a>
+                            </div>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="assets/img/img-post.jpg" alt="imagem do blog">
+                            </figure>
+                            <div>
+                                <h3>Guia html5 para iniciantes</h3>
+                                <time datetime="2017-04-29T19:00">20 JUN 2016</time>
+                                <p>O HTML é uma linguagem de códigos usada para marcação e estruturação de conteúdo. Neste tutorial, irei falar um pouco sobre a estruturação básica das páginas web utilizando HTML.</p>
+                                <a href="#">Post completo</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </main>
+        <footer class="footer">
+            <p>Design e desenvolvimento <span>feito por</span> Lucas de Oliveira</p>
+        </footer>
     </body>
 </html>
